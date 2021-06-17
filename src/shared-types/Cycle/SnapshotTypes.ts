6fc1fc5e7708696c1cfe14bd993e67674b4690b2
jairajdev <jairajranamagar.dev@gmail.com>
2021-06-17 19:26:54 +0630

    Remove the shared-types folder

--- src/shared-types/Cycle/SnapshotTypes.ts
+++
@@ -1,9 +0,0 @@
-import { NetworkHash } from '../State';
-
-/** TYPES */
-
-export interface Record {
-  networkDataHash: NetworkHash[];
-  networkReceiptHash: NetworkHash[];
-  networkSummaryHash: NetworkHash[];
-}
