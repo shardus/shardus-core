6d2620d5b934977583880f9aac9be995003abf07
municorn <municorn@musicaloft.com>
2023-08-18 11:04:07 -0500

    p2p/Join: Add comments to fields of `JoinRequestResponse`

--- src/p2p/Join.ts
+++ src/p2p/Join.ts
@@ -388,8 +388,13 @@ export async function createJoinRequest(
 }
 
 export interface JoinRequestResponse {
+  /** Whether the join request was accepted. TODO: consider renaming to `accepted`? */
   success: boolean
+
+  /** A message explaining the result of the join request. */
   reason: string
+
+  /** Whether the join request could not be accepted due to some error, usually in validating a join request. TODO: consider renaming to `invalid`? */
   fatal: boolean
 }
 export function addJoinRequest(joinRequest: P2P.JoinTypes.JoinRequest): JoinRequestResponse {
