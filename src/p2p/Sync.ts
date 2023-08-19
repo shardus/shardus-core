d621dd49e14d9038655b1db92bb34ad81f53b2a2
municorn <municorn@musicaloft.com>
2023-08-19 15:41:21 -0500

    p2p/Sync: Compute `standbyNodeListHash` for cycle records

--- src/p2p/Sync.ts
+++ src/p2p/Sync.ts
@@ -16,6 +16,7 @@ import { profilerInstance } from '../utils/profiler'
 import { nestedCountersInstance } from '../utils/nestedCounters'
 import { byJoinOrder } from './NodeList'
 import { addStandbyNodes } from './Join/v2'
+import * as JoinV2 from './Join/v2'
 
 /** STATE */
 
@@ -297,6 +298,7 @@ export function digestCycle(cycle: P2P.CycleCreatorTypes.CycleRecord, source: st
   if (config.p2p.useSyncProtocolV2 || config.p2p.writeSyncProtocolV2) {
     cycle.nodeListHash = NodeList.computeNewNodeListHash()
     cycle.archiverListHash = Archivers.computeNewArchiverListHash()
+    cycle.standbyNodeListHash = JoinV2.computeNewStandbyListHash()
   }
 
   const marker = CycleCreator.makeCycleMarker(cycle)
