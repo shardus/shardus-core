44e11a62bf2f8c2224c7a9ff86dae3cae4f706fd
Ahmed <iaahmed40@gmail.com>
2023-08-22 15:27:01 -0500

    removed repeated code

--- src/shardus/index.ts
+++ src/shardus/index.ts
@@ -2120,6 +2120,7 @@ class Shardus extends EventEmitter {
     const prunedData: WrappedData[] = await this.app.pruneNetworkChangeQueue(account, lastCycle.counter)
     await this.stateManager.checkAndSetAccountData(prunedData, 'global network account update', true)
 
+
     if (appData) {
       const data: WrappedData[] = await this.app.updateNetworkChangeQueue(account, appData)
       await this.stateManager.checkAndSetAccountData(data, 'global network account update', true)
@@ -2127,6 +2128,7 @@ class Shardus extends EventEmitter {
 
     this.p2p.configUpdated()
     this.loadDetection.configUpdated()
+
     }
   }
 
