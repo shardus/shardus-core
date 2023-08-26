ef3795286233052beb03a9c01db91ca5aa46f36c
municorn <municorn@musicaloft.com>
2023-08-26 15:22:11 -0500

    p2p/Join/routes: Call `saveJoinRequest` on new gossip route

--- src/p2p/Join/routes.ts
+++ src/p2p/Join/routes.ts
@@ -14,6 +14,7 @@ import { isBogonIP } from '../../utils/functions/checkIP'
 import { isPortReachable } from '../../utils/isPortReachable'
 import { nestedCountersInstance } from '../../utils/nestedCounters'
 import { profilerInstance } from '../../utils/profiler'
+import { saveJoinRequest } from './v2'
 
 const cycleMarkerRoute: P2P.P2PTypes.Route<Handler> = {
   method: 'GET',
@@ -137,7 +138,10 @@ const gossipValidJoinRequests: P2P.P2PTypes.GossipHandler<P2P.JoinTypes.JoinRequ
   // do not forward gossip after quarter 2
   if (CycleCreator.currentQuarter > 2) return
 
-  // TODO: add join request to standby node list (not done yet)
+  // add the join request to the global list of join requests. this will also
+  // add it to the list of new join requests that will be processed as part of
+  // cycle creation to create a standy node list
+  saveJoinRequest(payload)
   Comms.sendGossip('gossip-valid-join-requests', payload, tracker, sender, NodeList.byIdOrder, false)
 }
 
