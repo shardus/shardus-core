75cede19111ae8436925ef1a71de045c4f5b3617
municorn <municorn@musicaloft.com>
2023-08-26 18:32:08 -0500

    p2p/Join/v2: Replace `provideActiveNodes` with existing function

--- src/p2p/Join/v2/acceptance.ts
+++ src/p2p/Join/v2/acceptance.ts
@@ -4,26 +4,17 @@ import { EventEmitter } from "events";
 import { Result } from "neverthrow";
 import * as http from '../../../http'
 import { getRandom } from "../../../utils";
-import { crypto } from "../../Context";
+import { crypto, p2p } from "../../Context";
 import { JoinedConsensor } from "@shardus/types/build/src/p2p/JoinTypes";
 
 const eventEmitter = new EventEmitter()
-
-let activeNodes = [];
-
-/** 
-  * Temporarily provides a node list that can be used until we have
-  * syncrhonized the node list.
-  */
-export function provideActiveNodes(newActiveNodes: P2P.P2PTypes.Node[]): void {
-  activeNodes = newActiveNodes
-}
 
 export function getEventEmitter(): EventEmitter {
   return eventEmitter
 }
 
 export async function confirmAcceptance(onCycleMarker: hexstring): Promise<Result<boolean, Error>> {
+  const activeNodes = p2p.state.getActiveNodes()
   // we need to query for the cycle record from a node to confirm that we were,
   // in fact, accepted during the cycle
   const randomNode = getRandom(activeNodes, 1)[0]
@@ -32,7 +23,7 @@ export async function confirmAcceptance(onCycleMarker: hexstring): Promise<Resul
   try {
     cycle = await getCycleFromNode(randomNode, onCycleMarker)
   } catch (err) {
-    return err(new Error(`error getting cycle from node ${randomNode.ip}:${randomNode.port}: ${err}`))
+    return err(new Error(`error getting cycle from node ${randomNode.externalIp}:${randomNode.externalPort}: ${err}`))
   }
 
   const ourPublicKey = crypto.getPublicKey()
@@ -40,9 +31,9 @@ export async function confirmAcceptance(onCycleMarker: hexstring): Promise<Resul
   return ok(included)
 }
 
-async function getCycleFromNode(node: P2P.P2PTypes.Node, cycleMarker: hexstring): Promise<P2P.CycleCreatorTypes.CycleRecord> {
+async function getCycleFromNode(node: P2P.NodeListTypes.Node, cycleMarker: hexstring): Promise<P2P.CycleCreatorTypes.CycleRecord> {
   const cycle: P2P.CycleCreatorTypes.CycleRecord =
-    await http.get(`http://${node.ip}:${node.port}/cycle-by-marker?marker=${cycleMarker}`)
+    await http.get(`http://${node.externalIp}:${node.externalPort}/cycle-by-marker?marker=${cycleMarker}`)
 
   return cycle
 }
