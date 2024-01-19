de0ed4bd0bf593ba0ad8447a3aa616063325b537
Kaung Myattthu <2962000kaungmyatthu@gmail.com>
2024-01-20 01:04:41 +0700

    wip: bin serialization migrate send_cachedAppData - rebased

---
+++ src/types/CachedAppData.ts
@@ -0,0 +1,49 @@
+import { VectorBufferStream } from '../utils/serialization/VectorBufferStream'
+
+export const cCachedAppData = 7 
+export const cCachedAppDataVersion = 1
+
+
+export type AppSerializedCachedAppData = {
+    cycle: number
+    appData: Buffer
+    dataID: string
+}
+
+export function serializeCachedAppData (
+    stream: VectorBufferStream, 
+    obj: AppSerializedCachedAppData,
+    root = false
+): void {
+    if(root) {
+        stream.writeUInt16(cCachedAppData)
+    }
+    stream.writeUInt16(cCachedAppDataVersion)
+    stream.writeUInt32(obj.cycle)
+    stream.writeBuffer(obj.appData)
+    stream.writeString(obj.dataID)
+}
+
+export function deserializeCachedAppData (
+  stream: VectorBufferStream,
+  root = false
+): AppSerializedCachedAppData {
+  if(root) {
+    const type = stream.readUInt16()
+    if(type !== cCachedAppData) {
+      throw new Error(`Expected ${cCachedAppData} but got ${type}`)
+    }
+  }
+  const version = stream.readUInt16()
+  if(version !== cCachedAppDataVersion) {
+    throw new Error(`Expected ${cCachedAppDataVersion} but got ${version}`)
+  }
+  const cycle = stream.readUInt32()
+  const appData = stream.readBuffer()
+  const dataID = stream.readString()
+  return {
+    cycle,
+    appData,
+    dataID
+  }
+}
