bc265f763a2164ee5929278cd890eb1fd1d8afc8
Kaung Myattthu <2962000kaungmyatthu@gmail.com>
2024-01-22 19:57:10 +0700

    feat: apply new refined patterns as part of bin serialization standard

--- src/types/CachedAppData.ts
+++ src/types/CachedAppData.ts
@@ -1,8 +1,5 @@
 import { VectorBufferStream } from '../utils/serialization/VectorBufferStream'
-
-export const cCachedAppData = 7 
-export const cCachedAppDataVersion = 1
-
+import { TypeIdentifierEnum } from './enum/TypeIdentifierEnum'
 
 export type CachedAppData = {
     cycle: number
@@ -16,9 +13,8 @@ export function serializeCachedAppData (
     root = false
 ): void {
     if(root) {
-        stream.writeUInt16(cCachedAppData)
+        stream.writeUInt16(TypeIdentifierEnum.cCachedAppData)
     }
-    stream.writeUInt16(cCachedAppDataVersion)
     stream.writeUInt32(obj.cycle)
     stream.writeBuffer(obj.appData)
     stream.writeString(obj.dataID)
@@ -26,18 +22,7 @@ export function serializeCachedAppData (
 
 export function deserializeCachedAppData (
   stream: VectorBufferStream,
-  root = false
 ): CachedAppData {
-  if(root) {
-    const type = stream.readUInt16()
-    if(type !== cCachedAppData) {
-      throw new Error(`Expected ${cCachedAppData} but got ${type}`)
-    }
-  }
-  const version = stream.readUInt16()
-  if(version !== cCachedAppDataVersion) {
-    throw new Error(`Expected ${cCachedAppDataVersion} but got ${version}`)
-  }
   const cycle = stream.readUInt32()
   const appData = stream.readBuffer()
   const dataID = stream.readString()
