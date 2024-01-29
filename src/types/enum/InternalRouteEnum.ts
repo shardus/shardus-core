d39316a933b5ba8d8d2c27bc886a70728a963de2
Kaung Myattthu <2962000kaungmyatthu@gmail.com>
2024-01-29 23:16:12 +0700

    feat: binary serialized get_account_data_by_list

--- src/types/enum/InternalRouteEnum.ts
+++ src/types/enum/InternalRouteEnum.ts
@@ -4,4 +4,5 @@ export enum InternalRouteEnum {
   binary_send_cachedAppData = 'binary/send_cachedAppData', // tell
   binary_get_account_data_with_queue_hints = 'binary/get_account_data_with_queue_hints', // ask
   binary_get_account_queue_count = 'binary/get_account_queue_count', // ask
+  binary_get_account_data_by_list = 'binary/get_account_data_by_list', // ask
 }
