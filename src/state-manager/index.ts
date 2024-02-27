5416a0a709851973d2b99d6fb16a29da0286326f
Sonali Thakur <sonali@shardeum.org>
2024-02-27 18:20:30 +0530

    Migrate spread_tx_to_group_syncing endpoint

--- src/state-manager/index.ts
+++ src/state-manager/index.ts
@@ -1960,6 +1960,7 @@ class StateManager {
     this.p2p.unregisterInternal(InternalRouteEnum.binary_sync_trie_hashes)
     this.p2p.unregisterInternal(InternalRouteEnum.binary_compare_cert)
     this.p2p.unregisterInternal(InternalRouteEnum.binary_get_trie_hashes)
+    this.p2p.unregisterInternal(InternalRouteEnum.binary_spread_tx_to_group_syncing)
   }
 
   // //////////////////////////////////////////////////////////////////////////
