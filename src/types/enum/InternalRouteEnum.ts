b1b4b40881336d9564c72506862bd27a9ee1538f
Tanuj Soni <tanuj@shardeum.org>
2024-03-04 11:31:22 +0530

    Add get_cached_app_data binary serialization endpoint

--- src/types/enum/InternalRouteEnum.ts
+++ src/types/enum/InternalRouteEnum.ts
@@ -7,4 +7,5 @@ export enum InternalRouteEnum {
   binary_get_account_data_by_list = 'binary/get_account_data_by_list', // ask
   binary_broadcast_finalstate = 'binary/broadcast_finalstate', // tell
   binary_get_account_data = 'binary/get_account_data', // ask
+  binary_get_cached_app_data = 'binary/get_cached_app_data', // ask
 }
